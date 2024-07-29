c0b443b326426648e5d3e254c8dc956e09ef7642
APowichrowski <aleksander.powichrowski@gmail.com>
2024-07-29 08:21:17 +0200

    Init commit

---
+++ restaurant-data/src/test/java/com/ap/restaurant/RestaurantDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
